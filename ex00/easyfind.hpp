6a39998f62fdd5c1f79588b9a23a07b7dfaedcb4
Mohamed Elalj <moelalj@c1r2p8.1337.ma>
2025-01-06 17:17:11 +0100

    Done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 17:27:31 by moelalj           #+#    #+#             */
-/*   Updated: 2025/01/05 17:30:49 by moelalj          ###   ########.fr       */
+/*   Updated: 2025/01/06 17:16:28 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 # define EASYFIND_HPP
 
 #include <iostream>
-#include <exception>
+#include <algorithm>
+#include <vector>
 
 template <typename T>
-T easyfind(T container, int index){
-    
+void    easyfind(T cont, int num){
+    typename T::iterator it; //It is necessary in templates because the compiler cannot determine whether T::iterator is a type or a value without the typename keyword.
+
+    it = std::find(cont.begin(), cont.end(), num);
+    if (*it == num)
+        std::cout << "Found the number in the container: " << num << std::endl;
+    else
+        std::cout << "Cannot found the number " << num  << " in the container."<< std::endl;
 }
 #endif
